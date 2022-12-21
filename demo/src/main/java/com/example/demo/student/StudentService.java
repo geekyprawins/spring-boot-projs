8fc10c07cc60a2606aabc3578a6666de2f0dd7c8
geekyprawins <swamiraju.varma@learner.manipal.edu>
2022-12-21 17:50:29 +0530

    jpa repository setup

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -1,6 +1,7 @@
 package com.example.demo.student;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -9,9 +10,19 @@ import java.util.List;
 
 @Service
 public class StudentService {
+
+
+    private final StudentRepository studentRepository;
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(1L, "Praveen", "praveen@duck.com", LocalDate.of(2003, Month.MARCH, 31), 19)
-        );
+        return studentRepository.findAll();
+//        List.of(
+//                new Student(1L, "Praveen", "praveen@duck.com", LocalDate.of(2003, Month.MARCH, 31), 19)
+//        )
     }
 }
