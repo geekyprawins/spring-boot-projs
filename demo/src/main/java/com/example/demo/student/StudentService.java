56d2f0e2b47eeb0f71112447387eeb468ec37266
geekyprawins <swamiraju.varma@learner.manipal.edu>
2022-12-22 16:41:53 +0530

    update functionality

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -4,9 +4,11 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -41,6 +43,24 @@ public class StudentService {
         }
 
         studentRepository.deleteById(studentId);
+    }
+
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email){
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(()-> new IllegalStateException("student with id "+ studentId+ " does not exists."));
+        if(name != null && name.length()>0 && !Objects.equals(student.getName(),name)){
+            student.setName(name);
+        }
+        if(email != null && email.length()>0 && !Objects.equals(student.getEmail(),email)){
+            Optional<Student> optionalStudent = studentRepository.findStudentByEmail(email);
+            if(optionalStudent.isPresent()){
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
+
     }
 
 }
